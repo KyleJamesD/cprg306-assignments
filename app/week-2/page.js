7eb56f0afa63c3d43712877ad83ea728ad938c62
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-05-24 15:54:35 -0600

    First commit for week2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,12 @@
+import StudentInfo from "./student-info";
+import Link from 'next/link';
+export default function Page() {
+    return (
+      <main>
+              <Link href="/">Home </Link>
+        <h1>Shopping List</h1>
+        <StudentInfo  />
+      </main>
+    );
+  }
+  
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h1>Kyle Dyck</h1>
+      <Link href="https://github.com/KyleJamesD/cprg306-assignments">
+        https://github.com/KyleJamesD/cprg306-assignments
+      </Link>
+    </div>
+  );
+}
+
+
