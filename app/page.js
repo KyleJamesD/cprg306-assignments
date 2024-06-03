80a5eed81daae3b3a1df5834f279871286e934c9
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-06-03 08:30:43 -0600

    Completed Week 3 lab

--- app/page.js
+++ app/page.js
@@ -5,10 +5,15 @@ export default function Home() {
     <nav>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>     
       <Link href="/">Home </Link>
+      <br></br>
       <Link href="/about">About</Link>
       <br></br>
     <Link href="week-2">
         week-2
+      </Link>
+      <br></br>
+      <Link href="week-3">
+        week-3
       </Link>
   </nav>
   );
---
+++ app/week-3/page.js
@@ -0,0 +1,12 @@
+import ItemList from "./item-list";
+export default function Page(){
+    return(
+        <main>
+        <h1 className=" pt-3 pr-3 pl-3 text-3xl font-bold">Shopping List</h1>
+
+        <ItemList />
+
+        </main>
+    );
+
+}
