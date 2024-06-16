58a2170cba8cba0ed0892e1ba5393db9aa9bbc60
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2024-06-16 15:18:52 -0600

    completed Conditional Rendering of buttons

--- app/week-5/item.js
+++ app/week-5/item.js
@@ -2,9 +2,9 @@ export default function Item(props){
     return(
         <>
         <div className="p-3 bg-gray-900 w-80 m-2 ">
-        <li  className="font-bold text-2xl text-blue-600">{props.name}</li>
+        <p  className="font-bold text-2xl text-blue-600">{props.name}</p>
         {/*Buy and in are just text*/}
-        <li className="">Buy {props.quantity} in {props.category}</li>
+        <p className="">Buy {props.quantity} in {props.category}</p>
         </div>
 
 
